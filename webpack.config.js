1df754bf4cced144143af78eee3921b68a98b64c
Yasin Kuru <kuruyasin8@gmail.com>
2022-10-06 10:17:07 +0300

    webpack development mode supported added.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: process.env.NODE_ENV ?? "development",
   module: {
     rules: [
       {
